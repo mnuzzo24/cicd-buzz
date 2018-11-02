d9590072783e827642beaeafe0573d3600db387d
mnuzzo24 <mnuzzo24@yahoo.com>
2018-11-02 10:16:22 -0500

    adjusted circle tokens

--- .circleci/deploy_dockerhub.sh
+++ .circleci/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$CIRCLE_BRANCH" = "master" ]; then
     TAG="latest"
 else
-    TAG="$TRAVIS_BRANCH"
+    TAG="$CIRCLE_BRANCH"
 fi
-docker build -f Dockerfile -t "$TRAVIS_REPO_SLUG":$TAG .
-docker push "$TRAVIS_REPO_SLUG"
+docker build -f Dockerfile -t "$REPO_SLUG":$TAG .
+docker push "$REPO_SLUG"
