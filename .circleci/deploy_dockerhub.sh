cb99af898a8f763600b2edccb5fd229fea2f637e
mnuzzo24 <mnuzzo24@yahoo.com>
2018-11-05 16:18:30 -0600

    reconfig changes

--- .circleci/deploy_dockerhub.sh
+++ .circleci/deploy_dockerhub.sh
@@ -4,7 +4,7 @@ if [ "$CIRCLE_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$CIRCLE_BRANCH"
-fi
+fi 
 CIRCLE_SLUG=$CIRCLE_PROJECT_USERNAME"/"$CIRCLE_PROJECT_REPONAME
 docker build -f Dockerfile -t "$CIRCLE_SLUG":$TAG .
 docker push "$CIRCLE_SLUG"
